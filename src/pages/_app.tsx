b13cefed5b52b190d9c3b57882926cb51a5e330c
mooyg <mooybot@gmail.com>
2022-01-02 12:36:58 +0530

    fix: some seo configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,13 @@ const App = ({ Component, pageProps }: AppProps) => {
           locale: 'en_IE',
           url: 'https://mooy.vercel.app',
           site_name: 'mooy',
+          images: [
+            {
+              url: 'https://cdn.discordapp.com/avatars/679660057717375006/03b1206ec2c915e5da8224048e1a8749.png?size=512',
+            },
+          ],
         }}
+        description="Personal Space on the Internet for some Blogs and somethings about myself"
       />
       <Fonts />
       <Spotify />
