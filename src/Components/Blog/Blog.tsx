435cf4b1dc92d9eec8f3344078efbd4c474251bf
mooyg <mooybot@gmail.com>
2022-01-02 11:15:04 +0530

    fix: giving fix boxSize to  Container

--- src/Components/Blog/Blog.tsx
+++ src/Components/Blog/Blog.tsx
@@ -13,7 +13,7 @@ type BlogProps = {
 export const Blog = ({ posts }: BlogProps) => {
   return (
     <>
-      <Box boxSize={'lg'}>
+      <Box>
         <Heading p="4">Blog Posts</Heading>
         {posts.map((post, index) => {
           return <BlogCard key={index} frontmatter={post.frontmatter} slug={post.slug} />
