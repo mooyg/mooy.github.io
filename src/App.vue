8b861a509453287835d3641ac688a586e4629e21
mooyg <mooybot@gmail.com>
2023-05-06 22:02:14 +0530

    feat: migration to vite started gonna use vitepress ahead most likely

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black"
+  >
+    <div class="menu-bar">NAVIGATION BAR</div>
+    <div class="flex justify-center items-center h-full space-x-2">
+      <img src="/profile.jpg" class="rounded-xl w-20" />
+      <h1 class="text-6xl font-bold">Hi, I'm mooy.</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+.menu-bar {
+  position: absolute;
+  left: 45%;
+  bottom: 0;
+  margin: 10px;
+  background: rgba(145, 127, 179, 0.58);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(9.4px);
+  -webkit-backdrop-filter: blur(9.4px);
+  border: 1px solid rgba(145, 127, 179, 0.93);
+  font-weight: bold;
+  padding: 10px;
+}
+</style>
--- src/Components/ui/InlineCode.tsx
+++
@@ -1,6 +0,0 @@
-import { Code } from '@chakra-ui/react'
-import React, { ReactNode } from 'react'
-
-export const InlineCode = ({ children }: Record<'children', ReactNode>) => {
-  return <Code colorScheme={'telegram'}>{children}</Code>
-}
