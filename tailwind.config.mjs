5f1640c5d7eacede02d3736620811ddcb67c7a99
mooyg <mooybot@gmail.com>
2024-05-30 17:26:38 +0530

    feat: astro

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        rotate: "rotate 10s linear infinite",
+      },
+      keyframes: {
+        rotate: {
+          "0%": { transform: "rotate(0deg) scale(10)" },
+          "100%": { transform: "rotate(-360deg) scale(10)" },
+        },
+      },
+    },
+  },
+  presets: [require("@medusajs/ui-preset")],
+};
